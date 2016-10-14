c589f99d8c3a32251fbe8d7f104b63a4591b675b
Stanislaus Felix Andika <felix.zibond0341@gmail.com>
2016-10-14 14:45:45 +0700

    Update app.js
    update v2

--- app.js
+++ app.js
@@ -18,38 +18,72 @@ var btn = new mraa.Gpio(12);
 btn.dir(mraa.DIR_IN);
 
 // Routes
-app.get('/', function(req, res){
+app.get('/', function(req, res)
+{
     res.sendFile(__dirname+'/views/view.html');
 });
 
-app.get('/turn-on', function(req, res){
+app.get('/turn-on', function(req, res)
+{
     led.write(1);
     res.send('turned on');
 });
 
-app.get('/turn-off', function(req, res){
+app.get('/turn-off', function(req, res)
+{
     led.write(0);
     res.send('turned off');
 });
 
 // Socket
-io.on('connection', function(socket){
+io.on('connection', function(socket)
+{
     console.log('Socket connection openned');
     // socket event
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function()
+    {
        console.log('Socket connection closed');
     });
 });
 
-// function to detect btn state and send it to every clients
-function detect_button(){
-    state = btn.read();
-    io.emit('button state', state);
-    setTimeout(detect_button,100); // let's do it again after 1/10 second
-}
-detect_button(); // execute the function
+	// function to detect btn state and send it to every clients
+	function detect_button()
+	{
+	    state = btn.read();
+	    io.emit('button state', state);
+	    setTimeout(detect_button,100); // let's do it again after 1/10 second
+	    if(state == 1)
+	    {
+	    	led.write(1);
+	    	setTimeout(state,500);
+	    	led.write(0);
+	    	setTimeout(state,500);
+	    	led.write(1);
+	    	setTimeout(state,500);
+	    	led.write(0);
+	    	setTimeout(state,500);
+	    	led.write(1);
+	    	setTimeout(state,500);
+	    	led.write(0);
+	    	setTimeout(state,500);
+	    	led.write(1);
+	    	setTimeout(state,500);
+	    	led.write(0);
+	    	setTimeout(state,500);
+	    	led.write(1);
+	    	setTimeout(state,500);
+	    	led.write(0);
+	    	setTimeout(state,1500);
+	    		
+	    }else
+	    {
+	    	led.write(0);
+	    }
+	}
+	detect_button(); // execute the function
 
-// Listen
-http.listen(8080, function(){
-    console.log('Listening request from port 8080, press Ctrl+C to end it');
-});
+	// Listen
+	http.listen(8080, function()
+	{
+	    console.log('Listening request from port 8080, press Ctrl+C to end it');
+	});
